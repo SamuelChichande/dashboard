87b14239ae9095a1768c1c7911bb952b4abff853
SamuelChichande <sjchicha@espol.edu.ec>
2025-11-12 18:46:24 +0000

    Terminando Guia12

--- src/App.tsx
+++ src/App.tsx
@@ -54,13 +54,13 @@ function App() {
       <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
       {/* Gráfico */}
-      <Grid sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
 
       {/* Tabla */}
-      <Grid sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
 
       {/* Información adicional */}
-      <Grid>Elemento: Información adicional</Grid>
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
 
     </Grid>
   );
