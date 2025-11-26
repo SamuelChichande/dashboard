592e9dd056a1aea1c2ed336e4b608aecd882f133
SamuelChichande <sjchicha@espol.edu.ec>
2025-11-26 18:33:13 +0000

    Avance de la guia 13

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 */
 import './App.css'
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 import { Grid } from '@mui/material';
 
@@ -42,10 +44,10 @@ function App() {
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+      <Grid size={{ xs: 12, md: 12 }}><HeaderUI /></Grid>
 
       {/* Alertas */}
-      <Grid>Elemento: Alertas</Grid>
+      <Grid container justifyContent="right" alignItems="center"><AlertUI description="No se preveen lluvias" /></Grid>
 
       {/* Selector */}
       <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
