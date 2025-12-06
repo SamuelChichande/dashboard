8e1c8a70883e8460899b162fb32be388f2e9961c
SamuelChichande <sjchicha@espol.edu.ec>
2025-12-06 22:29:05 +0000

    Avance actividad

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-/*
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-*/
 import './App.css'
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
@@ -12,36 +7,6 @@ import useFetchData from './functions/useFetchData';
 
 import { Grid } from '@mui/material';
 
-/*
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-*/
 function App() {
 
   const dataFetcherOutput = useFetchData();
@@ -64,7 +29,7 @@ function App() {
           {dataFetcherOutput &&
             (<IndicatorUI
               title='Temperatura (2m)'
-              description={`${dataFetcherOutput.current.temperature_2m} ${dataFetcherOutput.current_units.temperature_2m}`} />)
+              description={`${dataFetcherOutput.data?.current.temperature_2m} ${dataFetcherOutput.data?.current_units.temperature_2m}`} />)
           }
         </Grid>
 
@@ -72,27 +37,24 @@ function App() {
           {dataFetcherOutput &&
             (<IndicatorUI
               title='Temperatura aparente'
-              description={`${dataFetcherOutput.current.apparent_temperature} ${dataFetcherOutput.current_units.apparent_temperature}`} />)
+              description={`${dataFetcherOutput.data?.current.apparent_temperature} ${dataFetcherOutput.data?.current_units.apparent_temperature}`} />)
           }
-          {/* IndicatorUI con la Temperatura aparente en °C' */}
         </Grid>
 
         <Grid size={{ xs: 12, md: 3 }}>
           {dataFetcherOutput &&
             (<IndicatorUI
               title='Velocidad del viento (10m)'
-              description={`${dataFetcherOutput.current.wind_speed_10m} ${dataFetcherOutput.current_units.wind_speed_10m}`} />)
+              description={`${dataFetcherOutput.data?.current.wind_speed_10m} ${dataFetcherOutput.data?.current_units.wind_speed_10m}`} />)
           }
-          {/* IndicatorUI con la Velocidad del viento en km/h' */}
         </Grid>
 
         <Grid size={{ xs: 12, md: 3 }}>
           {dataFetcherOutput &&
             (<IndicatorUI
               title='Humedad relativa (2m)'
-              description={`${dataFetcherOutput.current.relative_humidity_2m} ${dataFetcherOutput.current_units.relative_humidity_2m}`} />)
+              description={`${dataFetcherOutput.data?.current.relative_humidity_2m} ${dataFetcherOutput.data?.current_units.relative_humidity_2m}`} />)
           }
-          {/* IndicatorUI con la Humedad relativa en %' */}
         </Grid>
       </Grid>
 
--- src/functions/useFetchData.tsx
+++ src/functions/useFetchData.tsx
@@ -1,34 +1,39 @@
 import { useState, useEffect } from 'react';
 import { type OpenMeteoResponse } from '../types/DashboardTypes';
 
-export default function useFetchData(): OpenMeteoResponse | undefined {
-
+export default function useFetchData() {
+    
     const URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,wind_speed_10m,apparent_temperature&timezone=America%2FChicago';
 
     const [data, setData] = useState<OpenMeteoResponse>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string>();
 
     useEffect(() => {
 
         const fetchAPI = async () => {
+            setLoading(true);
+            setError("");
+
             try {
                 const response = await fetch(URL);
+
+                if (!response.ok) {
+                    throw new Error(`Error HTTP: ${response.status}`);
+                }
+
                 const json: OpenMeteoResponse = await response.json();
                 setData(json);
-            } catch (error) {
-                console.error("Error fetching data:", error);
+
+            } catch (e) {
+                setError(e instanceof Error ? e.message : 'Error desconocido');
+            } finally {
+                setLoading(false);
             }
         };
+
         fetchAPI();
+    }, []);
 
-        /*
-        fetch(URL)
-        .then(data => data.json())
-        .then(obj => {
-            setData(obj)
-        })
-        */
-
-    }, []); // El array vacío asegura que el efecto se ejecute solo una vez después del primer renderizado
-
-    return data;
+    return { data, loading, error };
 }
